94cb4ab8fb238febade237af3c71065164ebcd65
FredrikAndreasson <fredrik.andreasson@yandex.com>
2022-11-14 16:32:52 +0100

    Node/Tree base for supervision

--- OthelloMinMaxAI/MinMaxTree/Tree.cs
+++ OthelloMinMaxAI/MinMaxTree/Tree.cs
@@ -6,12 +6,14 @@ namespace OthelloMinMaxAI
 {
     class Tree
     {
+
         private Node root;
         private GameBoard gameState;
 
-        public Tree(GameBoard gameState)
+        public Tree(GameBoard gameState, Player player)
         {
             this.gameState = gameState;
+            root = new Node(gameState, new Move(), false, true, 0);
         }
 
         public Move GetMove()
